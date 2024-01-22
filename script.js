06c70595b07a4275d8da1230d031cca40aca8936
William Owalla <owallabilly@gmail.com>
2024-01-22 14:21:42 +0300

    Success page redirect set up

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const button = document.getElementById('btn');
+
+    button.addEventListener('click', (e)=> {
+        e.preventDefault();
+
+        const email = document.getElementById('email').value;
+        console.log(email);
+        if (email.trim() != "") {
+            email.value = ""
+
+            setTimeout(() => {
+                window.location.href = "success.html"
+            }, 500);
+        } 
+        
+    });
+});
---
+++ scs.js
@@ -0,0 +1,7 @@
+const delayTime = 3000;
+
+function redirectHome (){
+    window.location.href = "index.html";
+}
+
+setTimeout(redirectHome, delayTime);
