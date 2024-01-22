122709a7f9158db661e4bd127c8ae46ffb552657
William Owalla <owallabilly@gmail.com>
2024-01-22 15:37:44 +0300

    Implement redirect logic

--- scs.js
+++ scs.js
@@ -1,7 +1,19 @@
-const delayTime = 3000;
+const delayTime = 30000;
+let clicked = false;
 
 function redirectHome (){
     window.location.href = "index.html";
 }
 
-setTimeout(redirectHome, delayTime);
+setTimeout(() => {
+    if(!clicked){
+        redirectHome;
+    }
+}, delayTime);
+
+const scsButton = document.getElementById('scs-btn');
+scsButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    clicked = true;
+    window.location.href = "index.html"
+})
