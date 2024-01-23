ef21b6a5c3b59ca982a2a5165c1d86f5381b7bc1
William Owalla <owallabilly@gmail.com>
2024-01-23 11:09:36 +0300

    Update error handling logic

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=> {
+
     const button = document.getElementById('btn');
 
     button.addEventListener('click', (e)=> {
         e.preventDefault();
 
+        let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         const email = document.getElementById('email').value;
-        console.log(email);
-        if (email.trim() != "") {
-            email.value = ""
+        const error = document.getElementById('err');
 
-            setTimeout(() => {
-                window.location.href = "success.html"
-            }, 500);
-        } else { alert('Please enter a valid email'); }
-        
+        if (email.trim() != "" && email.match(regex)) {
+            error.style.display = 'none';
+            email.value = ""
+            window.location.href = "success.html"
+        } else { 
+            email.value = ""; 
+            error.style.display = 'block'; 
+        };
     });
 });
